d0399b604cd56fdd5bb7baf97e974c5ddda5f9c9
Cappuccinni <44321512+Cappuccinni@users.noreply.github.com>
2021-06-11 17:34:34 +0100

    Feedback 2. Adding popups to project

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
-let typer = document.getElementById('typer');
-let blink = true;
+// let typer = document.getElementById('typer');
+// let blink = true;
 
-setInterval(function(){ typer.style.opacity = '1'; document.getElementById('typer') = typer;}, 800);
-setTimeout(function(){ setInterval(function(){ typer.style.opacity = '0'; document.getElementById('typer') = typer;}, 800); }, 400);
+// setInterval(function(){ typer.style.opacity = '1'; document.getElementById('typer') = typer;}, 800);
+// setTimeout(function(){ setInterval(function(){ typer.style.opacity = '0'; document.getElementById('typer') = typer;}, 800); }, 400);
 
+$('.typed').typeWrite({
+    repeat: true,
+    interval: 4000,
+    speed: 8000,
+    color: "#fff"
+});
+
+document.querySelectorAll('.folio-link').addEventListener('mouseover', function(){
+
+});
